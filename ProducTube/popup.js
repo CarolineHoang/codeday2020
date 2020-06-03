6e6437fb1696a3980509151d4fd36b07f861eded
tiffenychen <tc2963@columbia.edu>
2020-06-03 19:52:43 -0400

    Add keyword working but not stored

--- ProducTube/popup.js
+++ ProducTube/popup.js
@@ -36,4 +36,32 @@ var display = function(block_name, title) {
   });
 
   /*COPIED CODE*/
-  
+var x = 0;
+var array = Array();
+
+function add_element_to_array()
+{
+ array[x] = document.getElementById("text1").value;
+ alert("Keyword: " + array[x] + " added");
+ x++;
+ document.getElementById("text1").value = "";
+}
+
+function display_array()
+{
+   var e = "<hr/>";   
+    
+   for (var y=0; y<array.length; y++)
+   {
+     e += "" + (y+1) + ". " + array[y] + "<br/>";
+   }
+   document.getElementById("Result").innerHTML = e;
+}
+
+document.getElementById("button1").addEventListener("click", add_element_to_array);
+document.getElementById("button1").addEventListener("click", display_array);
+// document.getElementById("button2").addEventListener("click", display_array);
+/* 
+function myFunction(){
+  console.log('asd');
+} */
