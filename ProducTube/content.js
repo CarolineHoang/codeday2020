c32847965a4fbcb4a6c6acd6a1fbb066fd1b9951
Cremebrutale <caroline.hoang@columbiaspectator.com>
2020-06-04 22:10:08 -0400

    wrote alarm system and a method to add keywords from a given page, pausing is finicky however

--- ProducTube/content.js
+++ ProducTube/content.js
@@ -1,23 +1,63 @@
 // alert("grrr")
 
+// chrome.runtime.onConnect.addListener(port => {
+//     console.log('connected ', port);
 
 chrome.runtime.onMessage.addListener( function (request, sender, sendResponse ){
     
 
-    const re = new RegExp('<h1 class=', 'gi')
-    const matches = document.documentElement.innerHTML.match(re) //|| []
-    document.documentElement.innerHTML.match(re)
-    var video = document.querySelector("video");
-    var title = document.querySelector("title") ;
-    var titleString = document.querySelector("title").innerHTML ;
-    console.log("testingtesting123 ")
-    console.log("video ", video, "title", title, "titleString", titleString)
-    sendResponse({count: matches , divContent: `wowowo`})
+    // const re = new RegExp('<h1 class=', 'gi')
+    // const matches = document.documentElement.innerHTML.match(re) //|| []
+    // document.documentElement.innerHTML.match(re)
+    // var video = document.querySelector("video");
+    // var title = document.querySelector("title") ;
+    // var titleString = document.querySelector("title").innerHTML ;
+    // console.log("testingtesting123 ")
+    // console.log("video ", video, "title", title, "titleString", titleString)
+
+    // chrome.storage.sync.set({key: value}, function() {
+    //     console.log('Value is set to ' + value);
+    //   });
+
+
+
+
+    
     console.log(request)
+    if (request.message == "send_matches"){
+        const re = new RegExp('<h1 class=', 'gi')
+        const matches = document.documentElement.innerHTML.match(re) //|| []
+        document.documentElement.innerHTML.match(re)
+        
+        sendResponse({count: matches , divContent: `wowowo`})
+    }
     if (request.message == "pause_video"){
-        console.log("new+tab here")
+        var video = document.querySelector("video");
+        // var title = document.querySelector("title") ;
+        // var titleString = document.querySelector("title").innerHTML ;
+        // console.log("testingtesting123 ")
+        console.log("video ", video )//, "title", title, "titleString", titleString)
+
+        console.log("pausing")
         video.pause();
+        sendResponse({video: video})
+    }
+    if (request.message == "get_video_info"){
+        // var video = document.querySelector("video");
+        var channelName = document.getElementById("channel-name").querySelector("a").innerHTML//alternatively: .querySelector("#text").getElementsByTagName("a")         //.getElementsByTagName("div")//.getElementById("text")//.getElementsByTagName("a").innerHTML;
+        var title = document.querySelector("title") ;
+        var titleString = document.querySelector("title").innerHTML ;
+        console.log("testingtesting123 ")
+        console.log(/*"video ", video,*/ "title", title, "titleString", titleString, "channelName:", channelName)
+        
+        console.log("new+tab here")
+        sendResponse({vidTitle: titleString, channelName: channelName})
+        // video.pause();
+    }
+    if (request.message == "print_test"){
+        console.log("PRINTF: " , request.printMsg)
     }
     // alert(request)
+    return Promise.resolve("Dummy response to keep the console quiet");
 })
 
--- ProducTube/popup.js
+++ ProducTube/popup.js
@@ -1,15 +1,72 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('button').addEventListener('click', onclick, false)
+    // document.querySelector('button').addEventListener('click', onclick, false)
+    // document.getElementsByTagName('button').addEventListener('click', onclick, false)
 
-    function onclick(){
+    articles = document.getElementsByTagName('button');
+    for (var i = 0; i < articles.length; i++) {
+        articles[i].addEventListener('click',onclick,false);
+    }
+
+    function onclick(event){
+        // e = e || window.event;
+        // var target = e.target || e.srcElement, text = target.textContent || target.innerText; 
+
+
         chrome.tabs.query({currentWindow: true, active:true },
         function( tabs ){
-            chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount )
+            // chrome.tabs.sendMessage(tabs[0].id, 'test', setCount )
+
+
+            chrome.tabs.sendMessage(tabs[0].id, {"message": "print_test", "printMsg": event.target.id}  ) //sends info to content.js and prints whatever 'this' is
+            chrome.tabs.sendMessage(tabs[0].id, {"message": "print_test", "printMsg": "[" +event.target.id+"] || [start_timer]:" }  )
+            // chrome.tabs.sendMessage(tabs[0].id, {"message": "print_test", "printMsg": "[" +event.target.id+"] || [start_timer]:"+event.target.id=="start_timer" }  )
+            chrome.tabs.sendMessage(tabs[0].id, {"message": "print_test", "printMsg": event.target.id=="start_timer" }  )
+
+            switch (event.target.id){
+                case "add_new_keyword":
+                    chrome.runtime.sendMessage({"message": "ADD_KEYWORD", "keyword": "cats"} , tester2 ) //sends keyword to background.js to add to the keyword list
+                    //this might be able to be accomplished right here actually
+                    chrome.storage.sync.set({keywords: [["cat".toUpperCase , Date.now(), 1 ]] }, function() {
+                        console.log('Value is set to ' + value);
+                      });
+                    break;
+                case "delete_keyword":
+                    chrome.runtime.sendMessage({"message": "DELETE_KEYWORD", "keyword": "cats" } , tester2 ) //sends keyword to background.js to delete from the keyword list
+                    //this might be able to be accomplished right here actually
+                    break;
+                case "start_timer":
+                    chrome.runtime.sendMessage({"message": "START_TIMER", "duration": 1, "alarm_name": "PRODUCTIVE_MODE" } , tester2 ) 
+                    chrome.tabs.sendMessage(tabs[0].id, {"message": "print_test", "printMsg": "set the alarm1"}  )
+                    chrome.alarms.create( "PRODUCTIVITY_MODE", { delayInMinutes: 0.25 //, periodInMinutes: 0.1 
+                    });
+                    window.close();
+                    chrome.tabs.sendMessage(tabs[0].id, {"message": "print_test", "printMsg": "set the alarm2"}  )
+                    //this 1) sets the alarm and 2) changes the storage value for whether we're in productivity mode or not to true (value used for reference if we will pause video )
+                    break;
+                case "stop_timer":
+                    chrome.runtime.sendMessage({"message": "STOP_TIMER" } , tester2 ) 
+                    chrome.alarms.clear("PRODUCTIVITY_MODE");
+                    window.close();
+                    //this 1) clears ALL alarms (not refined further rn) and 2) changes the storage value for whether we're in productivity mode or not to false (value used for reference if we will pause video )
+                    break;
+            }
+
+            chrome.tabs.sendMessage(tabs[0].id, {"message": "send_matches"} , setCount ) //sends info to content.js
+            chrome.runtime.sendMessage({"message": "test"} , tester ) //sends info to background.js
         })
     }
     function setCount (res) {
         const div = document.createElement('div')
         div.textContent = `${res.count} instances:: ${res.divContent}`
         document.body.appendChild(div)
+    }
+    function tester (res) {
+        console.log("clicking")
+        const div = document.createElement('div')
+        div.textContent = ` hidiho ${res.testVal} |||  ${res.divContent}`
+        document.body.appendChild(div)
+    }
+    function tester2 (res) {
+        console.log("adding new keyword.......")
     }
 }, false)
